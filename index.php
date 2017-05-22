3fe4eb32750c975d2a3d45a58e9f4182ace5a786
oprosbox <oprosbox@yandex.ru>
2017-05-22 09:35:24 +0300

    старт

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        $name1="это php1<p>";
+        $name2="это php2<p>";
+        echo $name1;
+        echo $name2;
+        
+        function createDb($login,$password)
+        {
+            
+        }
+        
+        ?>
+    </body>
+</html>
